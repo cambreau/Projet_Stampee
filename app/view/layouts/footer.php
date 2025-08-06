a1195308ea97901d6e2a55ac401b74dc922c9f73
cambreau <breau.camille76@gmail.com>
2025-08-06 12:03:37 -0400

    Creation du footer

---
+++ app/view/layouts/footer.php
@@ -0,0 +1,29 @@
+</main>
+<footer class="pied-de-page">
+  <picture class="logo">
+    <img src="{{asset}}/images/logo-stampee.webp" alt="Logo de STAMPEE" />
+  </picture>
+  <div class="pied-de-page__navigation">
+    <nav class="pied-de-page__navigation-principale">
+      <ul class="pied-de-page__menu">
+        <li><a class="lien" href="#">Accueil</a></li>
+        <li><a class="lien" href="#">À propos</a></li>
+        <li><a class="lien" href="#">Nos timbres</a></li>
+        <li><a class="lien" href="#">Demander d'expertise</a></li>
+        <li><a class="lien" href="#">Profil</a></li>
+      </ul>
+    </nav>
+    <nav class="pied-de-page__navigation-secondaire">
+      <ul class="pied-de-page__menu">
+        <li><a class="lien" href="#">Mentions légales</a></li>
+        <li><a class="lien" href="#">Signaler un problème</a></li>
+        <li><a class="lien" href="#">Nous contacter</a></li>
+      </ul>
+    </nav>
+  </div>
+  <p class="pied-de-page__copyright">
+    © 2025 Projet Patrimoine Philatélique — Tous droits réservés
+  </p>
+</footer>
+</body>
+</html>
